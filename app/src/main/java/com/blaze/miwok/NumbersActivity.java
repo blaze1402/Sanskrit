3d015c7390d513d06eeebab418f2e16889a9fd5f
Vijay Kumar Singh <vks14022000@gmail.com>
2021-03-01 17:49:41 +0530

    Added words in NumbersActivity with ArrayList and showed them through for loop statement

--- app/src/main/java/com/blaze/miwok/NumbersActivity.java
+++ app/src/main/java/com/blaze/miwok/NumbersActivity.java
@@ -1,8 +1,12 @@
 package com.blaze.miwok;
 
+import android.os.Bundle;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
+import java.util.ArrayList;
 
 public class NumbersActivity extends AppCompatActivity {
 
@@ -10,5 +14,27 @@ public class NumbersActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_numbers);
+
+        //Array List of numbers in english
+        ArrayList<String> words = new ArrayList<String>();
+
+        words.add("One");
+        words.add("Two");
+        words.add("Three");
+        words.add("Four");
+        words.add("Five");
+        words.add("Six");
+        words.add("Seven");
+        words.add("Eight");
+        words.add("Nine");
+        words.add("Ten");
+
+        LinearLayout rootView = findViewById(R.id.rootView);
+
+        for (int index = 0; index < words.size(); index++) {
+            TextView wordView = new TextView(this);
+            wordView.setText(words.get(index));
+            rootView.addView(wordView);
+        }
     }
 }
